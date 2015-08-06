a7eb77a8f4ddddac0536b993c6a7d271fe3246c3
Cirola2000 <cirobaroneto@gmail.com>
2015-08-06 20:40:31 +0200

    added back-end proxy

--- dynamiclod-node/public/javascripts/dynamiclod.js
+++ dynamiclod-node/public/javascripts/dynamiclod.js
@@ -1,6 +1,6 @@
 var main = angular.module('main', ["ngRoute"]);
 
-var serverURL = "http://localhost:9090/dynlod/";
+var serverURL = "http://localhost:9090/dynlod/api";
 
 
 main.config(['$routeProvider',
@@ -9,12 +9,12 @@ main.config(['$routeProvider',
       when('/quick', {
       templateUrl: '/partial/quick',
       controller: 'quickCtrl'
-    }).      
-    when('/home', {
+    }).
+      when('/home', {
       templateUrl: '/partial/home',
       controller: 'homeCtrl'
-    }).      
-    when('/wiki', {
+    }).
+      when('/wiki', {
       templateUrl: '/partial/wiki',
       controller: 'wikiCtrl'
     }).
@@ -22,64 +22,74 @@ main.config(['$routeProvider',
       redirectTo: '/home'
     });
   }]);
-  
+
 main.controller('quickCtrl', ['$scope', '$http', function ($scope, $http) {
   $scope.serverURL = serverURL;
   $scope.formats = [
-    {name:"turtle", format:"ttl"}, 
-    {name:"rdf/xml", format:"rdfxml"}, 
-    {name:"n3", format:"n3"}
-    ];
-     
-  $scope.datasetAddress = "http://datahub.io/dataset/news-100-nif-ner-corpus";
-  
-  $scope.startAPI = function(){
-    $scope.apiAddress = serverURL+"api?addDataset="+$scope.datasetAddress+"&rdfFormat="+$scope.choosenFormat.format;
-    $scope.showApiCall = true;
-    
-    $http.get($scope.apiAddress).
-      then(function(response) {
-        $scope.apiResponse = response.data.coreMsg;
-        $scope.showApiResponse = true; 
-     }, function(response) {
-       console.log(response);
-       $scope.apiResponse = "Error: "+response.data;
-       $scope.showApiResponse = true; 
+    { name: "turtle", format: "ttl" },
+    { name: "rdf/xml", format: "rdfxml" },
+    { name: "n3", format: "n3" }
+  ];
 
-     });
+  $scope.datasetAddress = "http://datahub.io/dataset/news-100-nif-ner-corpus";
+
+  $scope.startAPI = function () {
+    $scope.apiAddress = serverURL + "?addDataset=" + $scope.datasetAddress + "&rdfFormat=" + $scope.choosenFormat.format;
+    $scope.showApiCall = true;
+
+
+    $http.get("/partial/quick/proxy" + "?serverURL=" + serverURL + "?addDataset=" + $scope.datasetAddress + "&rdfFormat=" + $scope.choosenFormat.format).
+      then(function (response) {
+      if (typeof response.data.error == 'undefined') {
+        $scope.apiResponse = response.data.resp.coreMsg;
+        $scope.showApiResponseColor = "black";
+        $scope.showApiResponse = true;
+      }
+      else {
+        $scope.apiResponse = response.data.error.code;
+        $scope.showApiResponseColor = "red";
+        $scope.showApiResponse = true;
+
+      }
+    }, function (response) {
+        $scope.apiResponse = "Error: " + response.data;
+        $scope.showApiResponse = true;
+
+      });
+
   };
-  
-  $scope.checkStatus = function(){
-     $scope.showApiStatusCall = true;
-     $scope.apiStatusCall = $scope.serverURL+"api?datasetStatus="+$scope.datasetAddress;
-     
-     $http.get($scope.apiStatusCall).
-      then(function(response) {
-        console.log(response.data.distributions);
-        $scope.apiParserMessage = response.data.parserMsg;
-        $scope.distributions = response.data.distributions;
-        $scope.showApiStatusResponse = true; 
-     }, function(response) {
-       $scope.apiResponse = "Error: "+response;
-               $scope.showApiStatusResponse = true; 
-     });
+
+  $scope.checkStatus = function () {
+    $scope.showApiStatusCall = true;
+    $scope.apiStatusCall = $scope.serverURL + "?datasetStatus=" + $scope.datasetAddress;
+
+    $http.get("/partial/quick/proxy" + "?serverURL=" + serverURL + "?datasetStatus=" + $scope.datasetAddress).
+      then(function (response) {
+      // console.log(response.data.distributions);
+      $scope.apiParserMessage = response.data.resp.parserMsg;
+      $scope.distributions = response.data.resp.distributions;
+      $scope.showApiStatusResponse = true;
+    }, function (response) {
+        $scope.apiResponse = "Error: " + response;
+        $scope.showApiStatusResponse = true;
+      });
   };
-  
-  $scope.retrieveRDF = function(){
+
+  $scope.retrieveRDF = function () {
     $scope.showRDF = true;
-    
+
   };
-  
-  
-  
-  
+
+
+
+
 }]);
 
 main.controller('homeCtrl', ['$scope', '$http', function ($scope, $http) {
- 
+
 }]);
 
 
 main.controller('wikiCtrl', ['$scope', '$http', function ($scope, $http) {
-  
+
 }]);
--- dynamiclod-node/routes/quick.js
+++ dynamiclod-node/routes/quick.js
@@ -1,13 +1,24 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('partial/quick.html');
+});
+
+router.get('/proxy', function (req, res, next) {
+
+  console.log("Making request to: "+req.query.serverURL);
+
+  request(req.query.serverURL, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.send({resp: JSON.parse(body)});
+    }
+    else {
+      console.log(error);
+      res.send({error: error});
+    }
+  });
 });
 
 
