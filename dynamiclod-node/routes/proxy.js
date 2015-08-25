10eb28aa6e5573b43b1de2a2b4c332b0ac03a8ee
Cirola2000 <cirobaroneto@gmail.com>
2015-08-25 12:38:42 +0200

    added API status to statistic page

--- dynamiclod-node/routes/proxy.js
+++ dynamiclod-node/routes/proxy.js
@@ -3,8 +3,8 @@ var request = require('request');
 var router = express.Router();
 
 
-var serverURL = "http://localhost:9090/dynlod";
-// var serverURL = "http://vmdbpedia.informatik.uni-leipzig.de:9090/dynlod";
+// var serverURL = "http://localhost:9090/dynlod";
+var serverURL = "http://vmdbpedia.informatik.uni-leipzig.de:9090/dynlod";
 
 
 
