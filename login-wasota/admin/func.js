948608278ab6d4d999b39f34d8990ad78125e8b1
Moussallem <diegomoussallem@gmail.com>
2016-06-30 20:45:45 +0200

    admin pages

--- login-wasota/admin/func.js
+++ login-wasota/admin/func.js
@@ -11,3 +11,23 @@ $(function () {
   		$('.search-input').focus();
   	});
   });
+
+$(document).ready(function()
+{
+    var navItems = $('.admin-menu li > a');
+    var navListItems = $('.admin-menu li');
+    var allWells = $('.admin-content');
+    var allWellsExceptFirst = $('.admin-content:not(:first)');
+    
+    allWellsExceptFirst.hide();
+    navItems.click(function(e)
+    {
+        e.preventDefault();
+        navListItems.removeClass('active');
+        $(this).closest('li').addClass('active');
+        
+        allWells.hide();
+        var target = $(this).attr('data-target-id');
+        $('#' + target).show();
+    });
+});
