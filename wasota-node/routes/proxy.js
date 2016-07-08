ae258be98d89ac0eeb30f4cb10623c2e1bb87b25
cirola2000 <cirobaroneto@gmail.com>
2016-07-08 18:29:17 +0200

    -

--- wasota-node/routes/proxy.js
+++ wasota-node/routes/proxy.js
@@ -29,30 +29,19 @@ router.post('/*', function (req, res, next) {
   var requestParams =     {    
         method: 'POST',
         uri: query,
-        // hostname:'wasota.aksw.org',
         body: JSON.stringify(req.body),     
         headers: {
-          // 'Content-type': 'text/plain',
-        // 'User-Agent': 'curl/7.43.0'
         }
   }
-        
-    console.log("oppaa "+JSON.stringify(req.body));
   
   if(req.body.user !== undefined && req.body.password !== undefined){
-
-var auth = 'Basic ' + new Buffer(req.body.user + ':' + req.body.password ).toString('base64');
-
-// auth is: 'Basic VGVzdDoxMjM='
-
-// var header = {'Host': 'www.example.com', 'Authorization': auth};
-requestParams.headers.Authorization = auth;
-console.log(requestParams);
+    var auth = 'Basic ' + new Buffer(req.body.user + ':' + req.body.password ).toString('base64');
+    requestParams.headers.Authorization = auth;
+    console.log(requestParams);
   }
   
   request(
      requestParams
-    
   , function (error, response, body) {
     if (error) {
         return console.error('error:', error);
@@ -60,7 +49,6 @@ console.log(requestParams);
     console.log("API response: "+body);
     
     res.send((JSON.parse(body)));
-
   });
 });
 
@@ -68,14 +56,27 @@ console.log(requestParams);
 router.put('/*', function (req, res, next) {
   var query = wasotaAPI + req.url;
   console.log(query);
-  request(
-     { 
+  
+  
+  
+    var requestParams =     {    
         method: 'PUT',
         uri: query,
         body: JSON.stringify(req.body),     
-        headers: {'Content-type': 'text/plain',
-        'User-Agent': 'curl/7.43.0'}, 
-    }
+        headers: {
+        }
+  }
+  
+  if(req.body.user !== undefined && req.body.password !== undefined){
+    var auth = 'Basic ' + new Buffer(req.body.user + ':' + req.body.password ).toString('base64');
+    requestParams.headers.Authorization = auth;
+    console.log(requestParams);
+  }
+  
+  
+  
+  
+  request( requestParams
   , function (error, response, body) {
     if (error) {
         return console.error('error:', error);
